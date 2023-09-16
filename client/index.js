422a6dc3b69bf53ea0fd2433a222a06a4cd7155c
jdarmada <jdarmada@gmail.com>
2023-09-15 19:35:29 -0700

    first commit

---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StrictMode } from 'react';
+import { createRoot } from "react-dom/client"
+// import './stylesheets/styles.css';
+
+import App from "./App";
+
+
+const root = createRoot(document.getElementById('root'));
+root.render( <App/>)
+
+
